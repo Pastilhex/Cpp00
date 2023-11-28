def24210faed2d243686693c4f9c67f0e8ef77c7
pastilhex <ivodanielmarques@gmail.com>
2023-11-28 15:05:05 +0000

    Working in getters and setters

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -6,7 +6,49 @@
 /*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 11:40:28 by ialves-m          #+#    #+#             */
-/*   Updated: 2023/11/28 11:40:29 by ialves-m         ###   ########.fr       */
+/*   Updated: 2023/11/28 15:02:48 by ialves-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Contact.hpp"
+
+Contact::Contact(std::string fn, std::string ln, std::string nn, std::string pn, std::string ds){
+	this->_firstName = fn;
+	this->_lastName = ln;
+	this->_nickname = nn;
+	this->_phoneNumber = pn;
+	this->_darkestSecret = ds;
+}
+
+Contact::~Contact(){
+	return;
+}
+
+std::string	Contact::getFirstName(void) const{
+	return this->_firstName;	
+}
+
+std::string	Contact::getLastName(void) const{
+	return this->_lastName;	
+}
+
+std::string	Contact::getNickName(void) const{
+	return this->_nickname;	
+}
+
+std::string	Contact::getPhoneNumber(void) const{
+	return this->_phoneNumber;
+}
+
+std::string	Contact::getDarkestSecret(void) const{
+	return this->_darkestSecret;
+}
+
+void	Contact::setContact(std::string fname, std::string lname, std::string nname, std::string phnbr, std::string secret){
+	this->_firstName = fname;
+	this->_lastName = lname;
+	this->_nickname = nname;
+	this->_phoneNumber = phnbr;
+	this->_darkestSecret = secret;
+	return;
+}
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,7 +6,36 @@
 /*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 11:40:31 by ialves-m          #+#    #+#             */
-/*   Updated: 2023/11/28 11:40:32 by ialves-m         ###   ########.fr       */
+/*   Updated: 2023/11/28 15:01:41 by ialves-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CONTACT_H
+#define CONTACT_H
+
+#include <iostream>
+#include <cstring>
+
+class Contact {
+private:
+	std::string _firstName;		
+	std::string _lastName;
+	std::string _nickname;
+	std::string _phoneNumber;
+	std::string _darkestSecret;
+
+public:
+	Contact(std::string fn, std::string ln, std::string nn, std::string pn, std::string ds);
+	~Contact();
+
+	std::string getFirstName(void) const;
+	std::string getLastName(void) const;
+	std::string getNickName(void) const;
+	std::string getPhoneNumber(void) const;
+	std::string getDarkestSecret(void) const;
+	
+	void setContact(std::string fname, std::string lname, std::string nname, std::string phnbr, std::string secret);
+
+};
+
+#endif

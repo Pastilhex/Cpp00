105f19fbacc301c9c7bff7a78bcaf76ce69a9cb0
pastilhex <ivodanielmarques@gmail.com>
2023-12-03 21:40:06 +0000

    Working on ex02

---
+++ ex02/Account.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/03 19:49:06 by ialves-m          #+#    #+#             */
+/*   Updated: 2023/12/03 21:34:29 by ialves-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+static int	getNbAccounts( void )
+{
+	
+	return (0);
+}
+
+static int	getTotalAmount( void )
+{
+	return (0);	
+}
+
+static int	getNbDeposits( void )
+{
+	return (0);	
+}
+
+static int	getNbWithdrawals( void )
+{
+	return (0);	
+}
+
+static void	displayAccountsInfos( void )
+{
+
+}
